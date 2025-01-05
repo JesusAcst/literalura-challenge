da0408b2126372e0e6381704ed76d74d596ec122
Jesus Acosta <jesusdap4@gmail.com>
2025-01-05 15:24:44 -0500

    update

--- src/main/java/com/aluracursos/literalura_challenge/LiteraluraChallengeApplication.java
+++ src/main/java/com/aluracursos/literalura_challenge/LiteraluraChallengeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiteraluraChallengeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraChallengeApplication.class, args);
 	}
-
 }
